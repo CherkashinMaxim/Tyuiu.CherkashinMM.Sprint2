08426110e25ba685e9544268ce8636d4d58cceaf
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-10-04 18:05:33 +0500

    Task0

---
+++ Tyuiu.CherkashinMM.Sprint2.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.CherkashinMM.Sprint2.Task0.V5.Lib;
+
+namespace Tyuiu.CherkashinMM.Sprint2.Task0.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Check()
+        {
+            DataService ds = new DataService();
+
+            bool[] valid = [false, true, true, false, true, false];
+            int x = 105;
+            int y = 223;
+
+            CollectionAssert.AreEqual(valid, ds.GetCompareOperations(x,y));
+        }
+    }
+}
---
+++ Tyuiu.CherkashinMM.Sprint2.Task0.V5/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.CherkashinMM.Sprint2.Task0.V5.Lib;
+
+namespace Tyuiu.CherkashinMM.Sprint2.Task0.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Черкашин М. М. | ИИПб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Черкашин Максим Михайлович | ИИПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=) и       *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую последовательность  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+            x = 105;
+            y = 223;
+
+            Console.WriteLine($"x = {x}\ny={y}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool[] res = ds.GetCompareOperations(x, y);
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
