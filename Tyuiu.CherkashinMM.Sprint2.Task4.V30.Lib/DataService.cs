538e38ea5190ba5d53894ff3e4ffb668c605111f
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-10-10 21:30:12 +0500

    Task4

--- Tyuiu.CherkashinMM.Sprint2.Task4.V30.Lib/DataService.cs
+++ Tyuiu.CherkashinMM.Sprint2.Task4.V30.Lib/DataService.cs
@@ -6,6 +6,6 @@ public class DataService : ISprint2Task4V30
 {
     public double Calculate(double x, double y)
     {
-        return x * 2 > y ? Math.Pow(6 + (4 / (x * x)), y) : (y + (2 / (x * x)));
+        return x * 2 > y ? Math.Round(Math.Pow(6 + (4 / (x * x)), y), 3) : Math.Round(y + (2 / (x * x)), 3);
     }
 } 
